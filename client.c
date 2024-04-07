a9d7371b00f370edcb44d89c47bfb21d48a026f7
Bhautik Savaliya <bhautiksavaliya2000@gmail.com>
2024-04-07 14:52:52 -0400

    Server listening to multiple client and client sends request

--- client.c
+++ client.c
@@ -1,9 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/socket.h>
 #include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
 
-void main() {
+#define SERVER_PORT 9009
+#define LOCAL_IP 127.0.0.1
 
+int main() {
+    int sckt;
+    struct sockaddr_in server_addr;
+    char buffer[1024] = {0};
+    char *message = "Hello, server!";
+
+    // Creating the socket
+    if ((sckt = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        printf("\n Socket creation error \n");
+        return -1;
+    }
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(SERVER_PORT);
+
+    // // Convert IPv4 or IPv6 addresses from text to binary form
+    // if(inet_pton(AF_INET, LOCAL_IP, &server_addr.sin_addr) <= 0) {
+    //     printf("\nInvalid address / Address not supported \n");
+    //     return -1;
+    // }
+
+    // Making connection with server
+    if (connect(sckt, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
+        printf("\nConnection Failed \n");
+        return -1;
+    }
+
+    // Send a message to the server
+    send(sckt, message, strlen(message), 0);
+    printf("Message sent: %s\n", message);
+
+    // Receive a response from the server
+    int charread = read(sckt, buffer, sizeof(buffer));
+    if(charread > 0) {
+        printf("Server response: %s\n", buffer);
+    }
+    else {
+        printf("Failed to receive a response from the server\n");
+    }
+
+    // closing the socket
+    close(sckt);
+
+    return 0;
 }
--- server.c
+++ server.c
@@ -1,9 +1,98 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <sys/socket.h>
-#include <string.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/wait.h>
 
-void main() {
+#define PORT 9009
+
+void crequest(int client_sock) {
+    char buffer[1024];
+    int client_readChar;
+
+    // waiting for command from client in infinite loop
+    while(1) {
+        memset(buffer, 0, sizeof(buffer)); // resetting the buffer content.
+        
+        // Reading command sent from client
+        client_readChar = read(client_sock, buffer, sizeof(buffer));
+        if(client_readChar <= 0) {
+            perror("read_crequest");
+            break; 
+        }
+        
+        printf("Command received and processing....: %s\n", buffer);
+
+        sleep(10);
+        char response[1024];
+        snprintf(response, sizeof(response), "Server received: %s", buffer);
+
+        // Returning result back to client
+        write(client_sock, response, strlen(response));
+    }
+
+    close(client_sock); // Close the client socket
+}
+
+int main() {
+    int conn_sckt, new_socket;
+    struct sockaddr_in address;
+    int addrlen = sizeof(address);
+    pid_t child_pid;
+
+    // Creating socket file descriptor (connection socket)
+    if ((conn_sckt = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
     
+    // setting address attributes.
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+    
+    // Binding the socket with port number
+    if (bind(conn_sckt, (struct sockaddr *)&address, sizeof(address)) == -1) {
+        perror("bind_main");
+        exit(EXIT_FAILURE);
+    }
+    
+    if (listen(conn_sckt, 3) < 0) {
+        perror("listen_main");
+        exit(EXIT_FAILURE);
+    }
+    
+    printf("Server listening on port %d\n", PORT);
+    
+    while(1) {
+        if ((new_socket = accept(conn_sckt, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0) {
+            perror("accept_main");
+            continue; // Continue to the next iteration if accept fails
+        }
+        
+        // Fork a child process to handle the client's request
+        child_pid = fork();
+        
+        if(child_pid < 0) {
+            perror("fork failed");
+            close(new_socket);
+            continue;
+        }
+        
+        if(child_pid == 0) {
+            close(conn_sckt); // Close the listening socket in the child process
+            crequest(new_socket); // Handle the client's request
+            exit(0);
+        } 
+        else {
+            close(new_socket); // Close the connected socket in the parent process
+        }
+    }
+    
+    // Should never reach here
+    close(conn_sckt);
+    return 0;
 }
